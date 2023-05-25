a1090512bbf0c839b48cf0e9b0d2a7690820e814
Austine Gomez <austine.gomez@yahoo.com>
2023-05-24 22:38:21 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import './App.css'
+import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 function App() {
 
   return (
     <>
-
-<Container>
-  <Row>
-    <Col className="d-flex justify-content-center">
-      <Card className='card'>
-        <Card.Img src="./qr-code.png" className='card-img' />
-        <Card.Body>
-          <Card.Title style={{fontSize: '1.3rem', fontWeight: 'bold' }}>Improve your front-end skills by building projects</Card.Title>
-          <Card.Text style={{fontSize: '.9rem', color:'hsl(210, 3%, 55%)'}} className='text-center'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-  </Row>
-</Container>
-
-
-
-
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={10} md={6} lg={4}>
+            <Card className="card">
+              <Card.Img src="./qr-code.png" className="card-img" />
+              <Card.Body>
+                <Card.Title style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>Improve your front-end skills by building projects</Card.Title>
+                <Card.Text style={{ fontSize: '.9rem', color: 'hsl(210, 3%, 55%)' }} className="text-center">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
