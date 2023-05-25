f708a0d7c216e92e9da6a49e71e60d9bfeee659a
Austine Gomez <austine.gomez@yahoo.com>
2023-05-24 22:31:02 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Card.Img src="./qr-code.png" className='card-img' />
         <Card.Body>
           <Card.Title style={{fontSize: '1.3rem', fontWeight: 'bold' }}>Improve your front-end skills by building projects</Card.Title>
-          <Card.Text style={{fontSize: '1rem', color:'hsl(210, 3%, 55%)'}} className='text-center'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Card.Text>
+          <Card.Text style={{fontSize: '.9rem', color:'hsl(210, 3%, 55%)'}} className='text-center'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Card.Text>
         </Card.Body>
       </Card>
     </Col>
