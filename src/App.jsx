f11f363a13e5a3f116c205cf7526456556f8017d
Austine Gomez <austine.gomez@yahoo.com>
2023-05-24 22:30:09 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+<Container>
+  <Row>
+    <Col className="d-flex justify-content-center">
+      <Card className='card'>
+        <Card.Img src="./qr-code.png" className='card-img' />
+        <Card.Body>
+          <Card.Title style={{fontSize: '1.3rem', fontWeight: 'bold' }}>Improve your front-end skills by building projects</Card.Title>
+          <Card.Text style={{fontSize: '1rem', color:'hsl(210, 3%, 55%)'}} className='text-center'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  </Row>
+</Container>
+
+
+
+
     </>
   )
 }
