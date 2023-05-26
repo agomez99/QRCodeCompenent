2e2a3a94987ade338ee669aaed49fa74a15d6ed2
Austine Gomez <austine.gomez@yahoo.com>
2023-05-25 19:23:11 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
         <Row className="justify-content-center">
           <Col xs={10} md={6} lg={4}>
             <Card className="card">
-              <Card.Img src="./qr-code.png" className="card-img" />
+              <Card.Img src="./qr-code.png" className="card-img" alt="card-image" />
               <Card.Body>
                 <Card.Title style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>Improve your front-end skills by building projects</Card.Title>
-                <Card.Text style={{ fontSize: '.9rem', color: 'hsl(210, 3%, 55%)' }} className="text-center">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Card.Text>
+                <Card.Text style={{ fontSize: '15px', color: 'hsl(210, 3%, 55%)' }} className="text-center">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</Card.Text>
               </Card.Body>
             </Card>
           </Col>
